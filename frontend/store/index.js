6257d3ed4a94f04d145c9294bf908c2e632fb529
Bagas Udi Sahsangka <udibagas@gmail.com>
2020-07-08 12:42:52 +0700

    add method and mutation

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,10 +1,34 @@
 export const state = () => ({
-  listPerusahaan: []
+  listPerusahaan: [],
+  filterPerusahaan: [],
+  listSite: [],
+  filterSite: [],
+  listSensor: [],
+  filterSensor: [],
+  listUser: [],
+  filterUser: [],
+  statusFilter: [
+    { value: '0', text: 'Aktif' },
+    { value: '1', text: 'Non-Aktif' },
+  ]
 })
 
 export const mutations = {
   setListPerusahaan(state, data) {
     state.listPerusahaan = data
+    state.filterPerusahaan = data.map(l => { return { value: l.id, text: l.nama } })
+  },
+  setListSite(state, data) {
+    state.listSite = data
+    state.filterSite = data.map(l => { return { value: l.id, text: l.nama } })
+  },
+  setListSensor(state, data) {
+    state.listSensor = data
+    state.filterSensor = data.map(l => { return { value: l.id, text: l.nama } })
+  },
+  setListUser(state, data) {
+    state.listUser = data
+    state.filterUser = data.map(l => { return { value: l.id, text: l.name } })
   }
 }
 
@@ -12,5 +36,17 @@ export const actions = {
   async getListPerusahaan({ commit }) {
     let { data } = await this.$axios.get('/api/perusahaan/getList')
     commit('setListPerusahaan', data)
-  }
+  },
+  async getListSite({ commit }) {
+    let { data } = await this.$axios.get('/api/site/getList')
+    commit('setListSite', data)
+  },
+  async getListSensor({ commit }) {
+    let { data } = await this.$axios.get('/api/sensor/getList')
+    commit('setListSensor', data)
+  },
+  async getListUser({ commit }) {
+    let { data } = await this.$axios.get('/api/user/getList')
+    commit('setListUser', data)
+  },
 }
